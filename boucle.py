47478ab6f6e9961b1521cdcd11be549675006e74
balqisse-messaya <balqisse.messaya@laplateforme.io>
2024-11-04 11:18:34 +0100

    premier commit

---
+++ boucle.py
@@ -0,0 +1,33 @@
+for i in range(21) :
+    print(i)
+for i in range(0, 21, 2) : 
+    print(i)
+for i in range(1, 21) :
+    carre = i ** 2
+    print(f"Le carré de {i} est {carre}")
+
+
+
+N = int(input("entrez un entier superieur à zéro (N) :"));
+Z = 0
+while Z < 10 :
+    print(N," * ", Z," = ", Z*N)
+    Z = Z + 1 
+
+
+N = 1
+while N < 13:
+    print(N)
+    N += 1
+
+i = 0
+while i < 3:
+    print(i)
+    i += 1
+
+for i in range(3) :
+    print(i)
+
+
+
+
